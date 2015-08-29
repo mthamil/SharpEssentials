cd56caa693bd59de1dd76e9e753b0d72ebcb1e61
Matthew Hamilton <matthamilton@live.com>
2015-08-29 17:13:50 -0500

    Added Outercurve license attribution.

--- SharpEssentials.Testing.Controls/WPF/WpfTestCase.cs
+++ SharpEssentials.Testing.Controls/WPF/WpfTestCase.cs
@@ -1,5 +1,6 @@
 ﻿// Sharp Essentials
 // Copyright 2015 Matthew Hamilton - matthamilton@live.com
+// Copyright 2015 Outercurve Foundation
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
 
 using System;
 using System.Collections.Generic;
