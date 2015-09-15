8900edaef2ee2144b96c0a95a28e7a767a9e52dc
Matthew Hamilton <matthamilton@live.com>
2015-09-14 22:27:34 -0500

    Added extensions to await a WaitHandle like a Task.

---
+++ SharpEssentials.Tests.Unit/SharpEssentials/Concurrency/WaitHandleExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SharpEssentials.Concurrency;
+using Xunit;
+
+namespace SharpEssentials.Tests.Unit.SharpEssentials.Concurrency
+{
+    public class WaitHandleExtensionsTests : IDisposable
+    {
+        public WaitHandleExtensionsTests()
+        {
+            _handle = new ManualResetEvent(false);
+        }
+
+        [Fact]
+        public async Task Test_AsTask()
+        {
+            // Arrange.
+            var setTask = Task.Run(async () =>
+            {
+                await Task.Delay(200);
+                _handle.Set();
+            });
+
+            // Act.
+            await _handle.AsTask();
+
+            // Assert.
+            Assert.True(setTask.IsCompleted);
+        }
+
+        [Fact]
+        public async Task Test_AsTask_With_Timeout()
+        {
+            // Act/Assert.
+            await Assert.ThrowsAsync<TaskCanceledException>(() => 
+                _handle.AsTask(TimeSpan.FromMilliseconds(100)));
+        }
+
+        public void Dispose()
+        {
+            _handle.Dispose();
+        }
+
+        private readonly ManualResetEvent _handle;
+    }
+}
---
+++ SharpEssentials/Concurrency/WaitHandleExtensions.cs
@@ -0,0 +1,65 @@
+﻿// Sharp Essentials
+// Copyright 2015 Matthew Hamilton - matthamilton@live.com
+// Copyright 2013 Stephen Cleary
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharpEssentials.Concurrency
+{
+    /// <summary>
+    /// Contains utility methods that help in using <see cref="WaitHandle"/>s with
+    /// <see cref="Task"/>-based code.
+    /// </summary>
+    public static class WaitHandleExtensions
+    {
+        /// <summary>
+        /// Returns a <see cref="Task"/> that will complete when the given <see cref="WaitHandle"/> completes.
+        /// </summary>
+        /// <param name="handle">The handle to wait on.</param>
+        /// <remarks>See http://stackoverflow.com/questions/18756354/wrapping-manualresetevent-as-awaitable-task</remarks>
+        public static Task AsTask(this WaitHandle handle)
+        {
+            return AsTask(handle, Timeout.InfiniteTimeSpan);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="Task"/> that will complete when the given <see cref="WaitHandle"/> completes
+        /// or the <paramref name="timeout"/> expires.
+        /// </summary>
+        /// <param name="handle">The handle to wait on.</param>
+        /// <param name="timeout">The amount of time to wait for the handle to complete.</param>
+        /// <remarks>See http://stackoverflow.com/questions/18756354/wrapping-manualresetevent-as-awaitable-task</remarks>
+        public static Task AsTask(this WaitHandle handle, TimeSpan timeout)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            var registration = ThreadPool.RegisterWaitForSingleObject(handle, (state, timedOut) =>
+            {
+                var localTcs = (TaskCompletionSource<object>)state;
+                if (timedOut)
+                    localTcs.TrySetCanceled();
+                else
+                    localTcs.TrySetResult(null);
+            }, tcs, timeout, executeOnlyOnce: true);
+
+            tcs.Task.ContinueWith((_, state) =>
+                ((RegisteredWaitHandle)state).Unregister(null), 
+                    registration, TaskScheduler.Default);
+
+            return tcs.Task;
+        }
+    }
+}
