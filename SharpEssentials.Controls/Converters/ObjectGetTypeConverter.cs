07bdb1425b32f5a551d6f70c4a85495c34de498e
Matthew Hamilton <matthamilton@live.com>
2015-10-26 21:20:47 -0500

    Add a value converter that gets an objects' runtime type.

---
+++ SharpEssentials.Controls/Converters/ObjectGetTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SharpEssentials.Controls.Converters
+{
+    /// <summary>
+    /// An <see cref="IValueConverter"/> that produces an object's runtime type.
+    /// </summary>
+    public class ObjectGetTypeConverter : IValueConverter
+    {
+        /// <see cref="IValueConverter.Convert"/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.GetType() ?? typeof(object);
+        }
+
+        /// <see cref="IValueConverter.ConvertBack"/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
