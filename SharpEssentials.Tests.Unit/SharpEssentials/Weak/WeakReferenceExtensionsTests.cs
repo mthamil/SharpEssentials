379dc9f877f0af01be3402d49dac3133aada15cb
Matthew Hamilton <matthamilton@live.com>
2016-02-23 00:41:10 -0600

    Add Option wrapper method for WeakReference.

---
+++ SharpEssentials.Tests.Unit/SharpEssentials/Weak/WeakReferenceExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using SharpEssentials.Weak;
+using Xunit;
+
+namespace SharpEssentials.Tests.Unit.SharpEssentials.Weak
+{
+    public class WeakReferenceExtensionsTests
+    {
+        [Fact]
+        public void Test_TryGetTarget_When_Target_Not_Null()
+        {
+            // Arrange.
+            var target = new object();
+            var reference = new WeakReference<object>(target);
+
+            // Act.
+            var actual = reference.TryGetTarget();
+
+            // Assert.
+            Assert.True(actual.HasValue);
+        }
+
+        [Fact]
+        public void Test_TryGetTarget_When_Target_Null()
+        {
+            // Arrange.
+            var reference = new Func<WeakReference<object>>(() =>
+            {
+                var target = new object();
+                return new WeakReference<object>(target);
+            })();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            // Act.
+            var actual = reference.TryGetTarget();
+
+            // Assert.
+            Assert.False(actual.HasValue);
+        }
+    }
+}
---
+++ SharpEssentials/Weak/WeakReferenceExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Sharp Essentials
+// Copyright 2016 Matthew Hamilton - matthamilton@live.com
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+
+namespace SharpEssentials.Weak
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="WeakReference{T}"/>s
+    /// </summary>
+    public static class WeakReferenceExtensions
+    {
+        /// <summary>
+        /// Tries to retrieve the target object that is referenced by the current <see cref="T:System.WeakReference{T}"/> object.
+        /// </summary>
+        public static Option<T> TryGetTarget<T>(this WeakReference<T> reference) where T : class
+        {
+            T target;
+            return reference.TryGetTarget(out target)
+                ? target
+                : Option<T>.None();
+        } 
+    }
+}
