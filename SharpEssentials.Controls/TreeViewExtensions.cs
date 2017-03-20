826ce9dce965049ed39b1101842b96e63b876b02
Matthew Hamilton <matthamilton@live.com>
2017-03-20 14:58:06 -0500

    Fix doc string.

--- SharpEssentials.Controls/TreeViewExtensions.cs
+++ SharpEssentials.Controls/TreeViewExtensions.cs
@@ -18,39 +18,39 @@ using System.Windows.Controls;
 
 namespace SharpEssentials.Controls
 {
-	/// <summary>
-	/// Contains extension methods for <see cref="TreeView"/>s.
-	/// </summary>
-	public static class TreeViewExtensions
-	{
-		/// <summary>
-		/// Performs a depth-first search of the already generated items in a tree for a given item's
-		/// corresponding <see cref="TreeViewItem"/>.
-		/// </summary>
-		/// <param name="treeView">The tree to search</param>
-		/// <param name="item">The object to search for</param>
-		/// <returns>The corresponding <see cref="TreeViewItem"/> or <see cref="Option{TreeViewItem}.None"/></returns>
-		public static Option<TreeViewItem> FindContainerFromItem(this TreeView treeView, object item)
-		{
-			var found = FindGeneratedItem(treeView.ItemContainerGenerator, item);
-			return Option.From(found as TreeViewItem);
-		}
+    /// <summary>
+    /// Contains extension methods for <see cref="TreeView"/>s.
+    /// </summary>
+    public static class TreeViewExtensions
+    {
+        /// <summary>
+        /// Performs a depth-first search of the already generated items in a tree for a given item's
+        /// corresponding <see cref="TreeViewItem"/>.
+        /// </summary>
+        /// <param name="treeView">The tree to search</param>
+        /// <param name="item">The object to search for</param>
+        /// <returns>The corresponding <see cref="TreeViewItem"/> or <see cref="Option.None{TreeViewItem}()"/></returns>
+        public static Option<TreeViewItem> FindContainerFromItem(this TreeView treeView, object item)
+        {
+            var found = FindGeneratedItem(treeView.ItemContainerGenerator, item);
+            return Option.From(found as TreeViewItem);
+        }
 
-		/// <summary>
-		/// Attempts to find an item in a tree.
-		/// </summary>
-		private static object FindGeneratedItem(ItemContainerGenerator containerGenerator, object item)
-		{
-			var container = containerGenerator.ContainerFromItem(item);
-			if (container != null)
-				return container;
+        /// <summary>
+        /// Attempts to find an item in a tree.
+        /// </summary>
+        private static object FindGeneratedItem(ItemContainerGenerator containerGenerator, object item)
+        {
+            var container = containerGenerator.ContainerFromItem(item);
+            if (container != null)
+                return container;
 
-		    return containerGenerator.Items
-		                             .Select(containerGenerator.ContainerFromItem)
-		                             .Where(c => c != null)
-		                             .OfType<TreeViewItem>()
-		                             .Select(c => FindGeneratedItem(c.ItemContainerGenerator, item))
-		                             .FirstOrDefault(foundItem => foundItem != null);
-		}
-	}
+            return containerGenerator.Items
+                                     .Select(containerGenerator.ContainerFromItem)
+                                     .Where(c => c != null)
+                                     .OfType<TreeViewItem>()
+                                     .Select(c => FindGeneratedItem(c.ItemContainerGenerator, item))
+                                     .FirstOrDefault(foundItem => foundItem != null);
+        }
+    }
 }
